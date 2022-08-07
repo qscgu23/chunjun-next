8866308f647f1ccb9a30ee662cb658c46a75b5b7
qscgu23 <411705172@qq.com>
2022-08-07 16:41:44 +0800

    refactor(eslint): 代码格式化

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    sep: "_",
-    root_zh: "../docs_zh",
+    sep: '_',
+    root_zh: '../docs_zh',
+    sql: '../chunjun-examples/sql',
+    json: '../chunjun-examples/json'
   },
   i18n: {
-    locales: ["en-US", "zh-Hans"],
-    defaultLocale: "zh-Hans",
-  },
-};
+    locales: ['en-US', 'zh-Hans'],
+    defaultLocale: 'zh-Hans'
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
