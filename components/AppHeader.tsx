1137bc761c3d315d2edb9a298d0b03081f99f260
lemon <992273994@qq.com>
2022-08-10 09:26:14 +0800

    feat:导航active

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -12,7 +12,7 @@ import logo from '@/public/logo-dark.svg'
 import { headerLink, headerLinks } from '@/config/headerLinks'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { primaryColor } from '@/config/color'
 
 type Props = {
@@ -29,10 +29,20 @@ const AppHeader = (props: Props) => {
   type menutype = {
     item: headerLink
   }
+  useEffect(() => {
+    const { key } = router.query
+    setActiveKey(key as string)
+  }, [router.query.key])
   const MenuItem: FC<menutype> = ({ item }) => {
     return (
       <Link
-        href={item.path as string}
+        href={
+          {
+            pathname: item.path as string,
+            query: { key: item.key }
+          }
+        }
+
       >
         <div style={{
           borderColor: activeKey === item.key ? primaryColor.toString() : '',
@@ -54,18 +64,20 @@ const AppHeader = (props: Props) => {
           {pathlist.map((item, index) => {
             return (
               <>
-                <div
-                  style={{
+                <Link href={
+                  {
+                    pathname: item.path as string,
+                    query: { key: item.key }
+                  }}
+
+                >
+                  <a style={{
                     borderColor:
                       activeKey === item.key ? primaryColor.toString() : '',
                     borderBottomWidth: activeKey === item.key ? 2 : 0
                   }}
-                  className={` hover:text-black hover:bg-gray-100/50 w-[70px] text-[12px] flex justify-center items-center h-full text-gray-400 cursor-pointer`}
-                >
-                  <Link href={item.path as string}>
-                    <a className="uppercase">{item.name}</a>
-                  </Link>
-                </div>
+                    className={` hover:text-black hover:bg-gray-100/50 w-[70px] text-[12px] flex justify-center items-center h-full text-gray-400 cursor-pointer`}>{item.name}</a>
+                </Link>
                 {index !== pathlist.length - 1 && (
                   <div className="h-[50%] bg-gray-100 w-[1px]"></div>
                 )}
