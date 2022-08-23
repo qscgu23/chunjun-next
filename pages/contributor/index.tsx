2297a570e698ee5fd6ab7504178c6513ba79006e
qscgu23 <48400210+qscgu23@users.noreply.github.com>
2022-08-23 19:52:39 +0800

    refactor(contributor): 完善贡献者页面

--- pages/contributor/index.tsx
+++ pages/contributor/index.tsx
@@ -1,21 +1,44 @@
 import members from '@/githubInfo/members.json'
+import Aos from 'aos'
 import { Card, Text, Avatar, useMantineColorScheme } from '@mantine/core'
+import { useEffect } from 'react'
+import Image from 'next/image'
 
 const Contributor = () => {
   const { colorScheme } = useMantineColorScheme()
+  useEffect(() => {
+    Aos.init({
+      duration: 1000
+    })
+  }, [colorScheme])
   return (
     <main className="md:w-4/5 2xl:max-w-[60vw] mx-auto w-full md:py-8 py-6">
-      <Text className="font-roboto mb-4 text-3xl text-center capitalize">
-        Our contributors
+      <Text
+        className={`font-roboto mb-4 text-3xl capitalize flex items-center justify-center ${
+          colorScheme === 'dark' ? 'text-yellow-300' : 'text-black'
+        }`}
+      >
+        \(@^0^@)/ our contributors
+        <Image
+          src="/assets/svg/nice.svg"
+          height={50}
+          width={50}
+          alt="thanks"
+          priority
+          style={{ marginRight: 5 }}
+        />
       </Text>
       <Text className="mb-8 text-center font-bold">期待你的加入</Text>
-      <div className="grid md:grid-cols-4 grid-cols-1 md:gap-4 gap-2 md:p-0 px-4">
-        {members.map((member) => {
+      <div className="grid md:grid-cols-4 grid-cols-1 md:gap-4 gap-2 md:p-0 px-4 mb-12">
+        {members.map((member, index) => {
           return (
             <Card
+              data-aos="fade-up"
+              data-aos-delay={150 + index * 20}
               key={member.avatar_url}
               p="lg"
               radius="sm"
+              withBorder
               className={`hover:scale-110 transition-all duration-300 ${
                 colorScheme === 'light' ? 'bg-gray-50' : null
               }`}
@@ -44,6 +67,7 @@ const Contributor = () => {
           )
         })}
       </div>
+      <Text className="text-sm">ps.以上排名不分先后</Text>
     </main>
   )
 }
